783b1b52df5c22f756eea9c22f4e6a6087aa3942
diogorainhalopes <diogo.rainha.lopes@tecnico.ulisboa>
2022-06-01 01:15:10 +0100

    added torus debris shape

--- LAB2/src/entities/SpaceDebris.js
+++ LAB2/src/entities/SpaceDebris.js
@@ -14,7 +14,7 @@ class SpaceDebris extends SpaceEntity {
         var min = 1;
 
         var randomColor = '#'+('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
-        var randomShape = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+        var randomShape = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
         var randomWire = Math.random() < 0.5
 
         if (randomShape == 1) {
@@ -40,6 +40,13 @@ class SpaceDebris extends SpaceEntity {
             const material = new THREE.MeshBasicMaterial( { color: randomColor, wireframe: randomWire } );
             debris = new THREE.Mesh(geometry,material);
         }
+        if (randomShape == 5) {
+            const geometry = new THREE.TorusGeometry(PLANET_RADIUS/24, PLANET_RADIUS/24, 4, 8);
+            const material = new THREE.MeshBasicMaterial( { color: randomColor, wireframe: randomWire } );
+            debris = new THREE.Mesh(geometry,material);
+        }
+        
+
 
         debris.position.x = Math.random() * 2 - 1;
         debris.position.y = Math.random() * 2 - 1;
