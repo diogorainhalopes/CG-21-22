7f0760bbb58c2c7c9400ebf82b327a381c6cf41b
alravid <allancravid@tecnico.ulisboa.pt>
2022-06-10 21:53:17 +0100

    add press key events

--- LAB3/js/main.js
+++ LAB3/js/main.js
@@ -12,6 +12,22 @@ var topAng = false;
 var lateralAng = false;
 var defaultAng = false;
 
+var origamiFirstClockwise = false;
+var origamiFirstCounterclockwise = false
+var origamiSecondClockwise = false;
+var origamiSecondCounterclockwise = false
+var origamiThirdClockwise = false;
+var origamiThirdCounterclockwise = false
+
+var globalLighting = false;
+var shading = false;
+var lightingCalculation = false;
+
+var spotlightFirst = false;
+var spotlightSecond = false;
+var spotlightThird = false;
+
+
 const FLOOR_HEIGHT = 0.5;
 
 var palanque;
@@ -85,6 +101,55 @@ function onKeyDown(e) {
             lateralAng = true;
             indexCamera = 2;
             break;
+        case 119: // w
+        case 87:
+            origamiFirstClockwise = true;
+            break;
+        case 113: // q
+        case 81:
+            origamiFirstCounterclockwise = true;
+            break;
+        case 101: // e
+        case 69: 
+            origamiSecondClockwise = true;
+            break;
+        case 114: // r
+        case 82:
+            origamiSecondCounterclockwise = true;
+            break;
+        case 116: // t
+        case 84:
+            origamiThirdClockwise = true;
+            break;
+        case 121: // y
+        case 89:
+            origamiThirdCounterclockwise = true;
+            break;
+        case 97: // a 
+        case 65:
+            shading = true;
+            break;
+        case 115: // s
+        case 83:
+            lightingCalculation = true;
+            break;
+        case 100: // d
+        case 68:
+            globalLighting = true;
+            break;
+        case 122: // z
+        case 90:
+            spotlightFirst = true;
+            break;
+        case 120: // x
+        case 88: 
+            spotlightSecond = true;
+            break;
+        case 99: // c
+        case 67: 
+            spotlightThird = true;
+            break;
+
     }
 
 }
@@ -102,6 +167,56 @@ function onKeyUp(e) {
         case 51: // 3
             lateralAng = false;
             break;
+            case 119: // w
+            case 87:
+                origamiFirstClockwise = false;
+                break;
+            case 113: // q
+            case 81:
+                origamiFirstCounterclockwise = false;
+                break;
+            case 101: // e
+            case 69: 
+                origamiSecondClockwise = false;
+                break;
+            case 114: // r
+            case 82:
+                origamiSecondCounterclockwise = false;
+                break;
+            case 116: // t
+            case 84:
+                origamiThirdClockwise = false;
+                break;
+            case 121: // y
+            case 89:
+                origamiThirdCounterclockwise = false;
+                break;
+            case 97: // a 
+            case 65:
+                shading = false;
+                break;
+            case 115: // s
+            case 83:
+                lightingCalculation = false;
+                break;
+            case 100: // d
+            case 68:
+                globalLighting = false;
+                break;
+            case 122: // z
+            case 90:
+                spotlightFirst = false;
+                break;
+            case 120: // x
+            case 88: 
+                spotlightSecond = false;
+                break;
+            case 99: // c
+            case 67: 
+                spotlightThird = false;
+                break;
+    
+
     }
 }
 
